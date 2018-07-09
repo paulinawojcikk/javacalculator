7b0d4c496787a1db6c0a23825cfb88107a2549fc
Paulina Wójcik <paulina.wojcikk@o2.pl>
2018-07-09 13:24:50 +0200

    added lotto generator

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
@@ -5,7 +5,13 @@ package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.controller;
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Controller
 public class DescController {
@@ -17,6 +23,24 @@ public class DescController {
     @GetMapping("/desc")
     public String descGet(){
         return "desc";
+    }
+
+    //@ResponseBody
+    @GetMapping("/random")
+    public String getRandom(ModelMap map){
+        Set<Integer> lotto = new TreeSet<>();
+        Random random = new Random();
+
+        while (lotto.size() != 6) {
+            lotto.add(random.nextInt(49)+1);
+        }
+
+        map.put("lotto", lotto);  //dorzuca nazwę klucza i wartość
+
+
+
+
+        return "random"; //bez responsebody szuka pliku random.htm, który musi znajdować się w kataogu templates
     }
 
 }
